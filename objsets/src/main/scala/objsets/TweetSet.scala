f485a11609e42651ceeb8e6338956df64020250f
Can A. MOGOL <canmogol@gmail.com>
2016-07-09 19:12:29 +0300

    filter and union

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -7,8 +7,9 @@ import TweetReader._
  */
 class Tweet(val user: String, val text: String, val retweets: Int) {
   override def toString: String =
-    "User: " + user + "\n" +
-    "Text: " + text + " [" + retweets + "]"
+    ""+retweets
+    //"User: " + user + "\n" +
+    //"Text: " + text + " [" + retweets + "]"
 }
 
 /**
@@ -41,7 +42,7 @@ abstract class TweetSet {
    * Question: Can we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def filter(p: Tweet => Boolean): TweetSet = ???
+    def filter(p: Tweet => Boolean): TweetSet = filterAcc(p, new Empty)
   
   /**
    * This is a helper method for `filter` that propagetes the accumulated tweets.
@@ -54,7 +55,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-    def union(that: TweetSet): TweetSet = ???
+    def union(that: TweetSet): TweetSet 
   
   /**
    * Returns the tweet from this set which has the greatest retweet count.
@@ -107,7 +108,11 @@ abstract class TweetSet {
 }
 
 class Empty extends TweetSet {
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+    
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = acc 
+
+  def union(that: TweetSet): TweetSet = that
+
   
   /**
    * The following methods are already implemented
@@ -120,13 +125,22 @@ class Empty extends TweetSet {
   def remove(tweet: Tweet): TweetSet = this
 
   def foreach(f: Tweet => Unit): Unit = ()
+  
+  override def toString = "."
+
 }
 
 class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
 
-    def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = ???
+   
+  def filterAcc(p: Tweet => Boolean, acc: TweetSet): TweetSet = 
+        if(p(elem)) acc.incl(elem).union(left.filter(p)).union(right.filter(p))
+        else left.filter(p).union(right.filter(p))
+
+  def union(that: TweetSet): TweetSet = {
+        ((left union right) union that) incl elem
+  }
   
-    
   /**
    * The following methods are already implemented
    */
@@ -152,6 +166,9 @@ class NonEmpty(elem: Tweet, left: TweetSet, right: TweetSet) extends TweetSet {
     left.foreach(f)
     right.foreach(f)
   }
+
+  override def toString = "{" + left + elem + right + "}"
+
 }
 
 trait TweetList {
@@ -192,5 +209,31 @@ object GoogleVsApple {
 
 object Main extends App {
   // Print the trending tweets
-  GoogleVsApple.trending foreach println
+  //GoogleVsApple.trending foreach println
+  
+
+  val n1 = ct("n1","10",10, new Empty, new Empty)
+  val n2 = ct("n2","200",200, new Empty, new Empty)
+  val n3 = ct("n3","3000",3000, new Empty, new Empty)
+  
+  val n12 = n1.union(n2) 
+  val n123 = n12.union(n3)
+  println("n12  "+n12)
+  println("n123 "+n123)
+  
+
+  println("")
+  println("filter n1    "+n1.filter((t)=>(t.retweets<10)))
+  println("filter n1+   "+n1.filter((t)=>(t.retweets<11)))
+  println("")
+  println("filter n12   "+n12.filter((t)=>(t.retweets<200)))
+  println("filter n12+  "+n12.filter((t)=>(t.retweets<201)))
+  println("")
+  println("filter n123  "+n123.filter((t)=>(t.retweets<3000)))
+  println("filter n123+ "+n123.filter((t)=>(t.retweets<3001)))
+
+
+
+  def ct(user:String, text:String, retweets:Int, left:TweetSet, right:TweetSet) = new NonEmpty(new Tweet(user,text,retweets), left, right)
+
 }
